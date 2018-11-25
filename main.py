42f7898d12668e868a1f374125d5bd77a1db0be2
Sanket Gujar <sanketgujar95@gmail.com>
2018-11-24 23:28:56 -0500

    Ready for submission

--- main.py
+++ main.py
@@ -33,8 +33,15 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
+    graph = tf.get_default_graph()
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    w1 = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    w2 = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    w3 = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    w4 = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+    return w1, keep, w2, w3, w4
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,8 +54,36 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
-    return None
+    layer7_1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1,
+                                   padding= 'same',
+                                   kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                   kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    layer4_1 = tf.layers.conv2d_transpose(layer7_1, num_classes, 4,
+                                             strides= (2, 2),
+                                             padding= 'same',
+                                             kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                             kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    layer4_2 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1,
+                                   padding= 'same',
+                                   kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                   kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    layer4_3 = tf.add(layer4_1, layer4_2)
+    layer3_1 = tf.layers.conv2d_transpose(layer4_3, num_classes, 4,
+                                             strides= (2, 2),
+                                             padding= 'same',
+                                             kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                             kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    layer3_2 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1,
+                                   padding= 'same',
+                                   kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                   kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    layer3_3 = tf.add(layer3_1, layer3_2)
+    nn_last_layer_ = tf.layers.conv2d_transpose(layer3_3, num_classes, 16,
+                                               strides= (8, 8),
+                                               padding= 'same',
+                                               kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                               kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    return nn_last_layer_
 tests.test_layers(layers)
 
 
@@ -61,8 +96,13 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
-    return None, None, None
+    logits  = tf.reshape(nn_last_layer, (-1,num_classes))
+    labels_ = tf.reshape(correct_label,(-1,num_classes))
+    CE      = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = logits,
+                labels = labels_))
+    optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
+    train_op  = optimizer.minimize(CE)
+    return logits, train_op, CE
 tests.test_optimize(optimize)
 
 
@@ -81,8 +121,15 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
-    # TODO: Implement function
-    pass
+    sess.run(tf.global_variables_initializer())
+    for i in range(epochs):
+        for image,label in get_batches_fn(batch_size):
+            _,loss = sess.run([train_op, cross_entropy_loss],
+                              feed_dict= {input_image: image,
+                                         correct_label: label,
+                                         keep_prob: 0.5,
+                                         learning_rate:0.00091})
+            print ('Epoch ' , i, ' Loss: ', loss)
 tests.test_train_nn(train_nn)
 
 
@@ -110,11 +157,23 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
+        epoch = 50
+        batch_size = 5
+
+        label = tf.placeholder(tf.int32, [None, None, None, num_classes])
+        learning_rate = tf.placeholder(tf.float32, name = 'learning_rate')
+
+        input_image, keep_prob, v3_1, v4_1, v7_1 = load_vgg(sess, vgg_path)
+        nn_last_layer = layers(v3_1,v4_1,v7_1,num_classes)
+
+        logits, train_op, CEL = optimize(nn_last_layer,label, learning_rate, num_classes)
 
         # TODO: Train NN using the train_nn function
+        train_nn(sess, epoch, batch_size, get_batches_fn, train_op, CEL,
+        input_image, label, keep_prob, learning_rate)
 
         # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
         # OPTIONAL: Apply the trained model to a video
 
